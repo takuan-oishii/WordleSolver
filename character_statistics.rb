8e79b562f0fafc1f4ed76730869791c7419419a0
takuan-oishii <met.enkephalin@gmail.com>
2022-01-31 21:50:41 +0900

    初手に選ぶ単語候補出した

--- character_statistics.rb
+++ character_statistics.rb
@@ -17,13 +17,22 @@ end
   end
 end
 
-p
+count = count.to_a.sort_by { |a| a[1] }.reverse
+
+first_word_candidates = words.map do |word|
+  if count[0..3].map { |c| c[0] }.all? { |c| word.include? c }
+    word
+  else
+    nil
+  end
+end.compact
 
 File.open('character_statistics.result', 'w') do |f|
   f.puts "総単語数: #{words.count}"
   f.puts
   f.puts "使われてる文字ランキング"
-  count.to_a.sort_by { |a| a[1] }.reverse.each do |a|
-    f.puts "#{a[0]}: #{a[1]}語"
-  end
+  count.each { |a| f.puts "#{a[0]}: #{a[1]}語" }
+  f.puts
+  f.puts "初手に選ぶと強い単語候補"
+  first_word_candidates.each { |a| f.puts a }
 end
